18e7c6492560784ae9e57e2406a92dd5e6be6c11
monilprajapati <monillprajapatii@gmail.com>
2024-08-25 20:34:04 +0530

    Done with code

--- apps/server/src/services/socket.ts
+++ apps/server/src/services/socket.ts
@@ -1,5 +1,16 @@
 import { Server } from "socket.io";
+import Redis from "ioredis";
 
+const redisCongif = {
+  host: process.env.REDIS_HOST || "",
+  port: process.env.REDIS_PORT ? parseInt(process.env.REDIS_PORT) : "",
+  username: process.env.REDIS_USERNAME || "",
+  password: process.env.REDIS_PASSWORD || "",
+};
+
+const pub = new Redis(redisCongif);
+
+const sub = new Redis(redisCongif);
 // Socket service
 class SocketService {
   // Socket instance
@@ -8,7 +19,14 @@ class SocketService {
   // Constructor with socket instance
   constructor() {
     console.log("Socket service initialized");
-    this._io = new Server();
+    this._io = new Server({
+      cors: {
+        allowedHeaders: ["*"],
+        origin: "*",
+        methods: ["GET", "POST"],
+      },
+    });
+    sub.subscribe("MESSAGES");
   }
 
   // Function to initialize socket listeners
@@ -20,8 +38,18 @@ class SocketService {
 
       socket.on("event:message", async ({ message }: { message: string }) => {
         console.log("Message received: ", message);
-        // socket.emit("event:message", {message: "Message received"});
+        // publish message to redis
+        await pub.publish("MESSAGES", JSON.stringify({ message }));
       });
+
+      // Redis message listener for messages
+      sub.on("message", (channel, message) => {
+        if(channel === "MESSAGES"){
+        console.log("Message received from redis: ", message);
+        io.emit("message", JSON.parse(message));
+        }
+      });
+
     });
   }
 
--- apps/web/context/SocketProvider.tsx
+++ apps/web/context/SocketProvider.tsx
@@ -1,43 +1,64 @@
 "use client";
-
-import React, { createContext, useCallback, useContext } from "react";
+import React, { useCallback, useContext, useEffect, useState } from "react";
+import { io, Socket } from "socket.io-client";
 
 interface SocketProviderProps {
-  children: React.ReactNode;
+  children?: React.ReactNode;
 }
 
 interface ISocketContext {
-  sendMessage: (message: string) => any;
+  sendMessage: (msg: string) => any;
+  messages: string[];
 }
 
-const socketContext = createContext<ISocketContext | null>(null);
+const SocketContext = React.createContext<ISocketContext | null>(null);
 
-const SocketProvider: React.FC<SocketProviderProps> = ({ children }) => {
+export const useSocket = () => {
+  const state = useContext(SocketContext);
+  if (!state) throw new Error(`state is undefined`);
+
+  return state;
+};
+
+export const SocketProvider: React.FC<SocketProviderProps> = ({ children }) => {
+  const [socket, setSocket] = useState<Socket>();
+  const [messages, setMessages] = useState<string[]>([]);
+
   const sendMessage: ISocketContext["sendMessage"] = useCallback(
-    (message: string) => {
-      console.log("Sending message: ", message);
-      // socket.emit("event:message", { message });
+    (msg) => {
+      console.log("Send Message", msg);
+      if (socket) {
+        socket.emit("event:message", { message: msg });
+      }
     },
-    []
+    [socket]
   );
+
+  const onMessageRec = useCallback((msg: { message: string }) => {
+    console.log("From Server Msg Rec", msg);
+
+    // Directly access the message property since msg is already an object
+    const { message } = msg;
+
+    setMessages((prev) => [...prev, message]);
+  }, []);
+
+  useEffect(() => {
+    const _socket = io("http://localhost:4000");
+    _socket.on("message", onMessageRec);
+
+    setSocket(_socket);
+
+    return () => {
+      _socket.off("message", onMessageRec);
+      _socket.disconnect();
+      setSocket(undefined);
+    };
+  }, []);
 
   return (
-    <socketContext.Provider
-      value={{
-        sendMessage,
-      }}
-    >
+    <SocketContext.Provider value={{ sendMessage, messages }}>
       {children}
-    </socketContext.Provider>
+    </SocketContext.Provider>
   );
 };
-
-const useSocket = () => {
-  const context = useContext(socketContext);
-  if (!context) {
-    throw new Error("useSocket must be used within a SocketProvider");
-  }
-  return context;
-};
-
-export { SocketProvider, useSocket };
